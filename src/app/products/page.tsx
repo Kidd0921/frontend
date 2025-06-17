2d78b276b2e5c7537d02c994e488becc003a25a2
Kidd0921 <trevorhoh@gmail.com>
2025-06-17 18:48:16 +0800

    - product listing page todo: filters

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -1,3 +1,63 @@
-export default async function ProductList() {
-  return <h1>Products...</h1>
+import { Product } from "@/types/product";
+import ProductListing from "@/components/products/ProductListing";
+
+const PRODUCT_PER_PAGE = 8;
+const  products: Product[] = [
+  {
+    id: '1',
+    name: 'Classic White T-Shirt',
+    description: 'Soft cotton t-shirt with a relaxed fit.',
+    price: 29.99,
+    image: '/product/watch.jpeg',
+    category: 'Clothing',
+    rating: 4.5,
+    inStock: true,
+  },
+  {
+    id: '2',
+    name: 'Slim Fit Jeans',
+    description: 'Denim jeans with a modern slim fit cut.',
+    price: 59.99,
+    image: '/product/watch.jpeg',
+    category: 'Clothing',
+    rating: 4.2,
+    inStock: true,
+  },
+  {
+    id: '3',
+    name: 'Running Shoes',
+    description: 'Lightweight and comfortable running shoes.',
+    price: 89.99,
+    image: '/product/watch.jpeg',
+    category: 'Footwear',
+    rating: 4.8,
+    inStock: false,
+  },
+  {
+    id: '4',
+    name: 'Wireless Headphones',
+    description: 'Noise-cancelling over-ear headphones.',
+    price: 129.99,
+    image: '/product/watch.jpeg',
+    category: 'Electronics',
+    rating: 4.6,
+    inStock: true,
+  },
+  {
+    id: '5',
+    name: 'Old Headphones',
+    description: 'Old ear headphones.',
+    price: 6.99,
+    image: '/product/watch.jpeg',
+    category: 'Electronics',
+    rating: 2.6,
+    inStock: true,
+  },
+]
+const ListPage = () => {
+  return (
+    <ProductListing products={products}/>
+  )
 }
+
+export default ListPage
---
+++ src/components/products/ProductListing.tsx
@@ -0,0 +1,73 @@
+import { Product } from "@/types/product";
+import Link from "next/link";
+import Image from "next/image";
+import RatingStars from "@/components/RatingStars";
+import { ChevronDown } from 'lucide-react';
+
+interface ProductsListingProp {
+  products: Product[]; // example: 4.3
+}
+
+const ProductListing:React.FC<ProductsListingProp> = ({products}) => {
+  return (
+    <div className="mt-12 mb-20 px-2 flex flex-wrap gap-x-8 gap-y-16 justify-between">
+      {
+        products.map((product) => (
+          <div
+            className="relative w-full flex flex-col gap-4 sm:w-[45%] lg:w-[22%]"
+            key={product.id}
+          >
+            {/* Mobile full-cover Link */}
+            <Link
+              href="/test"
+              className="absolute inset-0 z-20 block sm:hidden"
+            >
+              {/* empty block just to cover */}
+            </Link>
+            <div className="relative w-full h-80">
+              <Link
+                href={"/test"}
+              >
+                <Image
+                  src={product.image}
+                  alt=""
+                  fill
+                  sizes="25vw"
+                  className="absolute object-cover rounded-md z-10"
+                />
+              </Link>
+            </div>
+            <Link
+              href={"/test"}
+              className="font-medium text-black hover:text-amber-500 transition-colors duration-200"
+            >
+              {product.name}
+            </Link>
+            
+            <div className="flex gap-x-3 items-center">
+              <span className="flex gap-x-1 items-center">
+                <RatingStars rating={product.rating}/>
+                <ChevronDown/>
+              </span>
+              
+              <span className="text-sm text-blue-500">101</span>
+            </div>
+
+            <Link
+              href={"/test"}
+              className="mt-4"
+            >
+              <div className="flex items-start space-x-1">
+                <span className="text-sm leading-0">$</span>
+                <span className="text-2xl font-semibold leading-0">{product.price}</span>
+              </div>
+            </Link>            
+
+          </div>
+        ))
+      }      
+    </div>
+  )
+}
+
+export default ProductListing
